7dd8ae3468c4266f27a09ce62f874168924e5a34
MouldyFoil <salisburyzoey213@gmail.com>
2025-09-05 20:46:01 -0700

    Exploring Visual Studio
    Basic project with an extra line and commented out code.

---
+++ GD14_1133_Lab1_salisbury_zoey/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GD14_1133_Lab1_salisbury_zoey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What are args?");
+            /*if (args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    Console.WriteLine("Whats " + arg + "?");
+                }
+            }
+            else
+            {
+                Console.WriteLine("An empty array.");
+            }*/
+        }
+    }
+}
