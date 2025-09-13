6d04eadf78a98efa3f16fe60e70190c1823f5ace
MouldyFoil <salisburyzoey213@gmail.com>
2025-09-12 22:03:28 -0700

    added various variables n stuff

---
+++ DiceGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using DiceGame.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ DiceGame/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame.Scripts
+{
+    internal class GameController
+    {
+        internal Character player = new();
+        Character opponent = new();
+        List<int> diceOptions;
+        
+    }
+    internal class Character
+    {
+        internal string name;
+        internal int score;
+        internal List<int> storedRolls;
+        internal List<int> diceOptionsLeft;
+    }
+}
