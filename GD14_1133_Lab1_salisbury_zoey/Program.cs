9216a8f9097238abcc9ffaa1fcbb53914500bc3b
MouldyFoil <salisburyzoey213@gmail.com>
2025-09-12 17:17:06 -0700

    Counted
    Made the program write numbers in various bases

--- GD14_1133_Lab1_salisbury_zoey/Program.cs
+++ GD14_1133_Lab1_salisbury_zoey/Program.cs
@@ -4,9 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello - Zoey September 12th 2025");
+            Console.WriteLine("Binary");
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+            Console.WriteLine("10");
+            Console.WriteLine("100");
+            Console.WriteLine("1000");
+            Console.WriteLine("10000");
+            Console.WriteLine("100000");
+            Console.WriteLine("1000000");
+            Console.WriteLine("1100100");
+            Console.WriteLine("11111111");
+
+            Console.WriteLine("Decimal");
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("4");
+            Console.WriteLine("8");
+            Console.WriteLine("16");
+            Console.WriteLine("32");
+            Console.WriteLine("64");
+            Console.WriteLine("100");
+            Console.WriteLine("255");
+
+            Console.WriteLine("Hexadecimal");
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("4");
+            Console.WriteLine("8");
+            Console.WriteLine("10");
+            Console.WriteLine("20");
+            Console.WriteLine("40");
+            Console.WriteLine("64");
+            Console.WriteLine("FA");
+            Console.WriteLine("Goodbye - Zoey September 12th 2025");
+            /*
+            Silly code for silly people
             Console.WriteLine("What are args?");
-            /*if (args.Length > 0)
+            if (args.Length > 0)
             {
                 foreach (string arg in args)
                 {
